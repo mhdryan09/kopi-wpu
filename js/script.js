8c9e72cc5c9d1f1036a2fde05c05e2fb4e8cb0f8
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2026-02-01 22:43:26 +0700

    feat: Menu Sidebar

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ document.querySelector("#hamburger-menu").onclick = () => {
 	// tampil class active
 	navbarNav.classList.toggle("active");
 };
+
+// Klik diluar Sidebar untuk menghilangkan Nav
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+	// selama yg diklik bukan navbar dan bukan hamburger
+	if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+		navbarNav.classList.remove("active");
+	}
+});
